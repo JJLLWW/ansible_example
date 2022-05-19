3d2097e137b7e508258ab0f5732667b7e5f9fb71
Jack Wright <jack.louie.wright@gmail.com>
2022-05-19 16:42:33 +0100

    fixed linker error

--- main.cpp
+++ main.cpp
@@ -2,17 +2,17 @@
 #include <omp.h>
 #include <vector>
 
-
 int main() {
   std::cout << "Bhjbzxjhvdsajgvdgasvjd" << std::endl;
   int sz = 100;
   std::vector<int> X(sz, 1);
   std::vector<int> Y(sz, 1);
   std::vector<int> Z(sz, 0);
-  /* is this  being parallelised? */
-  #pragma omp parallel for num_threads(4)
-  for(int i=0; i<sz; i++) {
-    std::cout << omp_get_thread_num() << std::endl;
-    Z[i] = X[i] + Y[i];
+  // our groundbreaking parallelised vector addition
+  #pragma omp parallel
+  {
+    for(int i=0; i<sz; i++) {
+      Z[i] = X[i] + Y[i];
+    }
   }
 }
